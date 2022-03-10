e9ce46d236cdab11b2e1ee47b8d4e69bc90eeb82
Ernest Fan <ernestf@evidentpoint.com>
2022-03-09 17:05:15 -0800

    Add wrapper class for DispatchSourceTimer

---
+++ Sources/DispatchSource/NYPLRepeatingTimer.swift
@@ -0,0 +1,62 @@
+//
+//  NYPLRepeatingTimer.swift
+//  NYPLUtilities
+//
+//  Created by Ernest Fan on 2022-03-09.
+//
+
+import Foundation
+
+private enum NYPLTimerState {
+  case resumed
+  case suspended
+}
+
+// This is a wrapper class of DispatchSourceTimer.
+// It keeps track of the state of the timer to avoid crash from
+// releasing, resuming or suspending the timer in the incorrect state.
+class NYPLRepeatingTimer {
+  
+  private var state: NYPLTimerState
+  
+  private var timer: DispatchSourceTimer
+  
+  init(interval: DispatchTimeInterval,
+       leeway: DispatchTimeInterval = .nanoseconds(0),
+       queue: DispatchQueue = DispatchQueue.global(),
+       handler: @escaping () -> Void) {
+    timer = DispatchSource.repeatingTimer(interval: interval, leeway: leeway, queue: queue, handler: handler)
+    state = .resumed
+  }
+  
+  deinit {
+    // Make sure timer is in the right state,
+    // releasing a suspended timer will cause a crash
+    // ref: https://developer.apple.com/forums/thread/15902?answerId=669654022#669654022
+    if state == .suspended {
+      timer.resume()
+    }
+  }
+  
+  func resume() {
+    // Make sure timer is in the right state,
+    // calling resume or suspend twice in a row will cause a crash
+    // ref: https://developer.apple.com/forums/thread/15902?answerId=669654022#669654022
+    guard state == .suspended else {
+      return
+    }
+    state = .resumed
+    timer.resume()
+  }
+  
+  func suspend() {
+    // Make sure timer is in the right state,
+    // calling resume or suspend twice in a row will cause a crash
+    // ref: https://developer.apple.com/forums/thread/15902?answerId=669654022#669654022
+    guard state == .resumed else {
+      return
+    }
+    state = .suspended
+    timer.suspend()
+  }
+}
